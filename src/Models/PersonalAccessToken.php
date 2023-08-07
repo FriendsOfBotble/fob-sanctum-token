9f730c855aa4bb902f28cd3a319e3b5e5984186d
Dinh Quoc Han <dqh@dinhquochan.com>
2023-08-07 22:16:16 +0700

    fix: compatible with new version of Botble (#3)

--- src/Models/PersonalAccessToken.php
+++ src/Models/PersonalAccessToken.php
@@ -2,8 +2,15 @@
 
 namespace Datlechin\SanctumToken\Models;
 
+use Botble\Base\Contracts\BaseModel;
+use Botble\Base\Models\Concerns\HasBaseEloquentBuilder;
+use Botble\Base\Models\Concerns\HasMetadata;
+use Botble\Base\Models\Concerns\HasUuidsOrIntegerIds;
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-class PersonalAccessToken extends SanctumPersonalAccessToken
+class PersonalAccessToken extends SanctumPersonalAccessToken implements BaseModel
 {
+    use HasMetadata;
+    use HasUuidsOrIntegerIds;
+    use HasBaseEloquentBuilder;
 }
